83d6a2d41c5aa6ea7ea263397824a2e73a1fb29a
Nik Zeman <nikzeman37@gmail.com>
2024-11-06 18:00:26 +0100

    ?

--- week05/excercises.cpp
+++ week05/excercises.cpp
@@ -48,17 +48,33 @@ int fibonacci(int idx) {
     int a=0;
     int b=1;
     int c;
+
     for(int i=0; i < idx; i++){
+        if(idx==0){
+        return a;
+        }
+        else if(idx==1){
+        return b;
+        }
+        else if(idx>1){
         c=a+b;
         a=b;
         b=c;
+        return (c);
+        }
     }
-    std::cout<<(c)<< std::endl;
-    return c;
+
+    /*
+    i-zacatek-a=0,,,,b=1,,,,c==?,,,
+    i-----1---a=1,,,,b=1,,,,c==1,,,
+    i-----2---a=1,,,,b=2,,,,c==2,,,
+    i-----3---a=2,,,,b=2,,,,c==3,,,
+    i-----4---a=0,,,,b=1,,,,c==1,,,
+    i-----5---a=0,,,,b=1,,,,c==1,,,*/
 }
 
 int main() {
-    std::cout<<hollow_rect(8, 8, 3)<< std::endl;
-    std::cout<<power(3,4)<< std::endl;
-    std::cout<<fibonacci(7)<< std::endl; 
+   // std::cout<<hollow_rect(8, 8, 3)<< std::endl;
+    //std::cout<<power(3,4)<< std::endl;
+    std::cout<<fibonacci(3)<< std::endl; 
 }
